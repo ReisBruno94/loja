c0fb4f91c1a3bf50ac4e3cce04135470771d8dd0
Bruno dos Reis Costa <bruno@eludica.com>
2018-08-27 14:19:04 -0300

    About

---
+++ sobre.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sobre:</title>
+</head>
+<body>
+	<h1>
+		<p class="text-success">Storage System for various purposes.</p><br>
+	</h1>
+	by Bruno.
+	</body>
+	</html>
